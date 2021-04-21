21b16e2e508e889fba15bf9f87c8d561ec9af3a0
Tal <talzhtc@gmail.com>
2021-04-21 13:43:45 +0300

    Added dummy implementation, needs testing

--- NumberWithUnits.cpp
+++ NumberWithUnits.cpp
@@ -6,3 +6,100 @@
 #include <iostream>
 
 #include "NumberWithUnits.hpp"
+
+using namespace ariel;
+using namespace std;
+
+NumberWithUnits::NumberWithUnits(double num, string unit){
+    
+}
+
+void NumberWithUnits::read_units(std::ifstream &u_fle){
+
+}
+
+//-----------------------------
+// Arithmetic Operators
+//-----------------------------
+
+NumberWithUnits NumberWithUnits::operator-(){
+    return *this;
+}
+
+NumberWithUnits NumberWithUnits::operator-(NumberWithUnits num){
+    return *this;
+}
+NumberWithUnits NumberWithUnits::operator-=(NumberWithUnits num){
+    return *this;
+}
+
+NumberWithUnits NumberWithUnits::operator+(){
+    return *this;
+}
+NumberWithUnits NumberWithUnits::operator+(NumberWithUnits num){
+    return *this;
+}
+NumberWithUnits NumberWithUnits::operator+=(NumberWithUnits num){
+    return *this;
+}
+
+//-----------------------------
+// Comparison Operators
+//-----------------------------
+
+bool NumberWithUnits::operator!=(NumberWithUnits num){
+    return false;
+}
+bool NumberWithUnits::operator==(NumberWithUnits num){
+    return false;
+}
+
+bool NumberWithUnits::operator<=(NumberWithUnits num){
+    return false;
+}
+bool NumberWithUnits::operator>=(NumberWithUnits num){
+    return false;
+}
+
+bool NumberWithUnits::operator<(NumberWithUnits num){
+    return false;
+}
+bool NumberWithUnits::operator>(NumberWithUnits num){
+    return false;
+}
+
+//-----------------------------
+// Increment Decrement Operators
+//-----------------------------
+
+NumberWithUnits NumberWithUnits::operator--(){
+    return *this;
+}
+NumberWithUnits NumberWithUnits::operator--(int dummy){
+    return *this;
+}
+
+NumberWithUnits NumberWithUnits::operator++(){
+    return *this;
+}
+NumberWithUnits NumberWithUnits::operator++(int dummy){
+    return *this;
+}
+
+//-----------------------------
+// Friend Operators
+//-----------------------------
+
+NumberWithUnits operator*(double d, NumberWithUnits num){
+    return num;
+}
+
+//-----------------------------
+// I/O Operators
+//-----------------------------
+ostream& operator<<(ostream &out, NumberWithUnits num){
+    return out;
+}
+istream& operator>>(istream &in, NumberWithUnits num){
+    return in;
+}
