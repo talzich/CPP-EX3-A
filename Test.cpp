240a9b75943642e3a064c12795362c48d0b0c95d
Tal <talzhtc@gmail.com>
2021-04-21 18:35:57 +0300

    Added tests

--- Test.cpp
+++ Test.cpp
@@ -1,3 +1,147 @@
 //
 // AUTHOR: Tal Zichlinsky
 // This is a test file made to test NumberWithUnits class and it's method
+
+#include <iostream>
+#include <fstream>
+
+#include "doctest.h"
+#include "NumberWithUnits.hpp"
+
+using namespace ariel;
+using namespace std;
+
+TEST_CASE("Construct before read"){
+
+    CHECK_THROWS( NumberWithUnits km1(1, "km") );
+    ifstream units_file{"units.txt"};
+    NumberWithUnits::read_units(units_file);
+    CHECK_NOTHROW( NumberWithUnits km1(1, "km") );
+}
+NumberWithUnits *km1;
+NumberWithUnits *m1;
+NumberWithUnits *cm1;
+NumberWithUnits *ton1;
+NumberWithUnits *kg1;
+NumberWithUnits *g1;
+NumberWithUnits *hour1;
+NumberWithUnits *min1;
+NumberWithUnits *sec1;
+NumberWithUnits *USD1;
+NumberWithUnits *ILS1;
+
+TEST_CASE("Initialize"){
+    ifstream units_file{"units.txt"};
+    NumberWithUnits::read_units(units_file);
+    km1 = new NumberWithUnits(1, "km");
+    m1 = new NumberWithUnits(1, "m");
+    cm1 = new NumberWithUnits(1, "cm");
+    ton1 = new NumberWithUnits(1, "ton");
+    kg1 = new NumberWithUnits(1, "kg");
+    g1 = new NumberWithUnits(1, "g");
+    hour1 = new NumberWithUnits(1, "hour");
+    min1 = new NumberWithUnits(1, "min");
+    sec1 = new NumberWithUnits(1, "sec");
+    USD1 = new NumberWithUnits(1, "USD");
+    ILS1 = new NumberWithUnits(1, "ILS");
+}
+
+TEST_CASE("Valid Comparison Operator"){
+
+    // Operator ==
+    CHECK(km1 == NumberWithUnits(1, "km"));
+    CHECK(m1 == NumberWithUnits(1, "m"));
+    CHECK(cm1 == NumberWithUnits(1, "cm"));
+    CHECK(ton1 == NumberWithUnits(1, "ton"));
+    CHECK(kg1 == NumberWithUnits(1, "kg"));
+    CHECK(g1 == NumberWithUnits(1, "g"));
+    CHECK(hour1 == NumberWithUnits(1, "hour"));
+    CHECK(min1 == NumberWithUnits(1, "min"));
+    CHECK(sec1 == NumberWithUnits(1, "sec"));
+    CHECK(USD1 == NumberWithUnits(1, "USD"));
+    CHECK(ILS1 == NumberWithUnits(1, "ILS"));
+
+    CHECK(km1 == NumberWithUnits(1000, "m"));
+    CHECK(m1 == NumberWithUnits(100, "cm"));
+    CHECK(ton1 == NumberWithUnits(1000, "kg"));
+    CHECK(kg1 == NumberWithUnits(1000, "g"));
+    CHECK(hour1 == NumberWithUnits(60, "min"));
+    CHECK(min1 == NumberWithUnits(60, "sec"));
+    CHECK(USD1 == NumberWithUnits(3.33, "ILS"));
+
+    CHECK(km1 == NumberWithUnits(100000, "cm"));
+    CHECK(ton1 == NumberWithUnits(1000000, "g"));
+    CHECK(hour1 == NumberWithUnits(3600, "sec"));
+
+    // Operator <=
+    CHECK(km1 <= NumberWithUnits(1000, "m"));
+    CHECK(km1 <= NumberWithUnits(2000, "m"));
+    CHECK(km1 <= NumberWithUnits(1, "km"));
+    CHECK(km1 <= NumberWithUnits(2, "km"));
+
+    CHECK(m1 <= NumberWithUnits(100, "cm"));
+    CHECK(m1 <= NumberWithUnits(1000, "cm"));
+    CHECK(m1 <= NumberWithUnits(1, "m"));
+    CHECK(m1 <= NumberWithUnits(2, "cm"));
+
+    CHECK(ton1 <= NumberWithUnits(1000, "kg"));
+    CHECK(ton1 <= NumberWithUnits(2000, "kg"));
+    CHECK(ton1 <= NumberWithUnits(1, "ton"));
+    CHECK(ton1 <= NumberWithUnits(2, "ton"));
+
+    CHECK(kg1 <= NumberWithUnits(1000, "g"));
+    CHECK(kg1 <= NumberWithUnits(2000, "g"));
+    CHECK(kg1 <= NumberWithUnits(1, "kg"));
+    CHECK(kg1 <= NumberWithUnits(2, "kg"));
+
+    CHECK(hour1 <= NumberWithUnits(60, "min"));
+    CHECK(hour1 <= NumberWithUnits(600, "min"));
+    CHECK(hour1 <= NumberWithUnits(1, "hour"));
+    CHECK(hour1 <= NumberWithUnits(2, "hour"));
+
+    CHECK(km1 <= NumberWithUnits(100000, "cm"));
+    CHECK(km1 <= NumberWithUnits(1000, "m"));
+    CHECK(km1 <= NumberWithUnits(2000, "m"));
+    CHECK(km1 <= NumberWithUnits(1, "km"));
+    CHECK(km1 <= NumberWithUnits(2, "km"));
+
+    // Operator >=
+    CHECK(km1 >= NumberWithUnits(100, "m"));
+    CHECK(km1 >= NumberWithUnits(200, "m"));
+    CHECK(km1 >= NumberWithUnits(1, "km"));
+    CHECK(km1 >= NumberWithUnits(0.2, "km"));
+
+    CHECK(m1 >= NumberWithUnits(10, "cm"));
+    CHECK(m1 >= NumberWithUnits(1000, "cm"));
+    CHECK(m1 >= NumberWithUnits(1, "m"));
+    CHECK(m1 >= NumberWithUnits(2, "cm"));
+
+    CHECK(ton1 >= NumberWithUnits(1000, "kg"));
+    CHECK(ton1 >= NumberWithUnits(2000, "kg"));
+    CHECK(ton1 >= NumberWithUnits(1, "ton"));
+    CHECK(ton1 >= NumberWithUnits(2, "ton"));
+
+    CHECK(kg1 >= NumberWithUnits(1000, "g"));
+    CHECK(kg1 >= NumberWithUnits(2000, "g"));
+    CHECK(kg1 >= NumberWithUnits(1, "kg"));
+    CHECK(kg1 >= NumberWithUnits(2, "kg"));
+
+    CHECK(hour1 >= NumberWithUnits(60, "min"));
+    CHECK(hour1 >= NumberWithUnits(600, "min"));
+    CHECK(hour1 >= NumberWithUnits(1, "hour"));
+    CHECK(hour1 >= NumberWithUnits(2, "hour"));
+
+    CHECK(km1 >= NumberWithUnits(100000, "cm"));
+    CHECK(km1 >= NumberWithUnits(1000, "m"));
+    CHECK(km1 >= NumberWithUnits(2000, "m"));
+    CHECK(km1 >= NumberWithUnits(1, "km"));
+    CHECK(km1 >= NumberWithUnits(2, "km"));
+
+
+}
+
+TEST_CASE("Arithmetic Operators"){
+        
+        // Unary minus
+        CHECK();
+}
