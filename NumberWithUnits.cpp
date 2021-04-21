23017d990e9347284e1ab913a16bd73dbb2a7e38
Tal <talzhtc@gmail.com>
2021-04-21 13:13:13 +0300

    Starting dummy implementation of class

---
+++ NumberWithUnits.cpp
@@ -0,0 +1,8 @@
+//
+// AUTHOR: Tal Zichlinsky
+// This file will be used to build the NumberWithUnits class, as described in this asignment's README file
+// (https://github.com/talzich/CPP-EX3-A/blob/master/README.md)
+
+#include <iostream>
+
+#include "NumberWithUnits.hpp"
